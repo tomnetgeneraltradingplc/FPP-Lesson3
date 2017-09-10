3ffa112edfec2d0266cdcd1b1c68e5a788afdb48
TOM <TOM@10.0.0.138>
2017-09-10 14:37:34 -0500

    Lesson 3

--- Lesson3/src/lesson3/problem3/MyDate.java
+++ Lesson3/src/lesson3/problem3/MyDate.java
@@ -1,4 +1,7 @@
 package lesson3.problem3;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
@@ -6,94 +9,23 @@ import java.util.Scanner;
 import lesson3.problem4.Circle;
 import lesson3.problem4.Rectangle;
 import lesson3.problem4.Triangle;
-public class MyDate {
-	private GregorianCalendar date = null; 
-	private String[] months = new String[]{ "january", "february", "march", "april", "may", 
-	                                "june", "july", "august", "september", "october", "november", "december" }; 
-	public MyDate(int month, int day, int year) { 
-	    date = new GregorianCalendar(year, month-1, day); 
-	} 
 
-	public MyDate(String month, int day, int year) { 
-	    date = new GregorianCalendar(year, this.getMonth(month), day); 
-	} 
-
-	public MyDate(int dayOfYear, int year) { 
-	    date = new GregorianCalendar(); 
-	    date.set(Calendar.DAY_OF_YEAR, dayOfYear); 
-	    date.set(Calendar.YEAR, year); 
-	} 
-	private int getMonth(String month) { 
-	    for (int i=0; i<months.length; ++i) 
-	      if (month.toLowerCase().equals(months)) //equals(months)) 
-	        return i; 
-	    return 0; 
-	} 
-	public String toString() { 
-	    return date.get(Calendar.MONTH)+1 + "-" + date.get(Calendar.DATE) + "-" + date.get(Calendar.YEAR); 
-	} 
-	public static void main(String[] args) {
-		
-        
-        
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter 1 for format: MM/DD/YYYY");
-		System.out.println("Enter 2 for format: Month DD, YYYY");
-		System.out.println("Enter 3 for format: DDD YYYY");
-		System.out.println("Enter 4 to exit");
-		System.out.println("----------------------\n");
-		String answer = scanner.nextLine();
-		switch (answer) {
-		case "1": {
-			System.out.println("Enter the month by number: ");
-			int month = scanner.nextInt();
-			System.out.println("Enter the day by number: ");
-			int day = scanner.nextInt();
-			System.out.println("Enter the year by number: ");
-			int year = scanner.nextInt();
-			MyDate d1 = new MyDate(month,day,year);
-			System.out.println("Area of Circle is " + circle.computeArea());
-			break;
-		}
-		case "2": {
-			System.out.println("Enter the width: ");
-			int width = scanner.nextInt();
-			System.out.println("Enter the height: ");
-			int height = scanner.nextInt();
-			Rectangle rectangle = new Rectangle(width, height);
-			System.out.println("Area of Rectangle is =" + rectangle.computeArea());
-			break;
-		}
-		case "3": {
-
-			System.out.println("Enter the base: ");
-			int base = scanner.nextInt();
-			System.out.println("Enter the height : ");
-			int height = scanner.nextInt();
-			Triangle triangle = new Triangle(base, height);
-			System.out.println("Area of Triangle is = " + triangle.computeArea());
-			break;
-		}
-		default:
-			System.out.println("You exited");
-			System.exit(4);
-			break;
-		}
+class MyDate {
+	public MyDate() {
 	}
 
+	public String getformat1(int month, int day, int year) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		return (dtf.format(LocalDate.of(year, month, day))).toString();
+	}
+
+	public String getformat2(int month, int day, int year) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMMM dd,yyyy");
+		return (dtf.format(LocalDate.of(year, month, day))).toString();
+	}
+
+	public String getformat3(int month, int day, int year) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("DDD yyyy");
+		return (dtf.format(LocalDate.of(year, month, day))).toString();
+	}
 }
-//		scanner.close();
-		
-	/*Output
-	 * Enter 1 for format: MM/DD/YYYY
-Enter 2 for format: Month DD, YYYY
-Enter 3 for format: DDD YYYY
-Enter 4 to exit
-------------------------------
-
-Choose your Choice: 
-*/	
-	 
-		
-
-
---
+++ Lesson3/src/lesson3/problem3/MyDateTest.java
@@ -0,0 +1,77 @@
+package lesson3.problem3;
+
+import java.util.Scanner;
+
+public class MyDateTest {
+	public static void main(String[] args) {
+		boolean continueExecution = false;
+		do {
+			System.out.println("Enter 1 for format : MM/DD/YYYY");
+			System.out.println("Enter 2 for format : Month DD,YYYY");
+			System.out.println("Enter 3 for format : DDD YYYY");
+			System.out.println("Enter 4 to exit");
+			System.out.println("Enter your choice :");
+			Scanner scanner = new Scanner(System.in);
+			String userSelection = scanner.nextLine();
+			switch (userSelection) {
+			case "1":
+				System.out.println("Choice 1");
+				System.out.println("Enter Month :");
+				int month = scanner.nextInt();
+				System.out.println("Enter Day");
+				int day = scanner.nextInt();
+				System.out.println("Enter Year");
+				int year = scanner.nextInt();
+				MyDate myDate = new MyDate();
+				System.out.println("MM/dd/yyyy : " + myDate.getformat1(month, day, year));
+				System.out.println("MMMM dd,yyyy : " + myDate.getformat2(month, day, year));
+				System.out.println("DDD yyyy : " + myDate.getformat3(month, day, year));
+				break;
+			case "2":
+				System.out.println("Choice 2");
+				System.out.println("Enter Month in text");
+				System.out.println("Enter Day");
+				System.out.println("Enter Year");
+				break;
+			case "3":
+				System.out.println("Choice 3");
+				System.out.println("Enter Day of Year");
+				System.out.println("Enter Year");
+				break;
+			case "4":
+				System.out.println("You Exit");
+				break;
+			
+			}
+			System.out.println("Do you want to Continue : ");
+			String usercontinueExecution = scanner.nextLine();
+			if (usercontinueExecution.equalsIgnoreCase("Y")) {
+				continueExecution = true;
+			} 
+			else {
+				continueExecution = false;
+			}
+		} while (continueExecution);
+		System.out.println("Terminated");
+	}
+}
+/*Output
+Enter 1 for format : MM/DD/YYYY
+Enter 2 for format : Month DD,YYYY
+Enter 3 for format : DDD YYYY
+Enter 4 to exit
+Enter your choice :
+1
+Choice 1
+Enter Month :
+12
+Enter Day
+28
+Enter Year
+2015
+MM/dd/yyyy : 12/28/2015
+MMMM dd,yyyy : December 28,2015
+DDD yyyy : 362 2015
+Do you want to Continue : 
+Terminated
+*/
